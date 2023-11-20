2e922b9c8bd4d4a2d583f998bb595e4d12f0e0ed
falahuddeen <115775463+falahuddeen@users.noreply.github.com>
2023-11-20 20:51:40 +0530

    [5th Commit] Creation of User Interface Part 01 Include ( footer.html, header.html, home.html, post.html, base.html ) and its views also css-style.css [mentioned both Updation or Creation]

--- iblogs/blog/urls.py
+++ iblogs/blog/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home
+from .views import home, post
 
 urlpatterns = [
-    path('home/',home)
+    path('home/',home),
+    path('blog/<slug:url>', post)
 ]
--- iblogs/blog/views.py
+++ iblogs/blog/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html', {})
+    #load all the post from db(10)
+    posts=Post.objects.all()[:11]
+    # print(posts)
+    data={
+        'posts':posts
+    }
+    return render(request,'home.html', data)
+
+def post(request, url):
+    post=Post.objects.get(url=url)
+    # print(post)
+    return render(request,'posts.html', {'post':post})
