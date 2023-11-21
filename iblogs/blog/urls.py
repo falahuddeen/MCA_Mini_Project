6e2045f7337c6d3d23f6008f565d64d4069939a4
falahuddeen <115775463+falahuddeen@users.noreply.github.com>
2023-11-21 21:36:07 +0530

    [7th Commit] Creation of User Interface Part 03 Include (footer mediam completion , header completion , drop down Category completion )

--- iblogs/blog/urls.py
+++ iblogs/blog/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home, post
+from .views import home, post, category
 
 urlpatterns = [
     path('home/',home),
-    path('blog/<slug:url>', post)
+    path('blog/<slug:url>', post),
+    path('category/<slug:url>', category)
 ]
--- iblogs/blog/views.py
+++ iblogs/blog/views.py
@@ -18,5 +18,11 @@ def home(request):
 
 def post(request, url):
     post=Post.objects.get(url=url)
+    cats = Category.objects.all()
     # print(post)
-    return render(request,'posts.html', {'post':post})
+    return render(request,'posts.html', {'post':post,'cats':cats})
+
+def category(request,url):
+    cat=Category.objects.get(url=url)
+    posts=Post.objects.filter(cat=cat)
+    return render(request,'category.html', {'cat':cat,'posts':posts})
